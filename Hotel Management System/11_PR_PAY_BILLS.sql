dfb7d96d26b7e97fd5d974ca7f9956e4fbe3aca3
Ragavan <ragav87@ymail.com>
2017-01-17 21:22:01 +0530

    Added Bill amount feature

--- Hotel Management System/11_PR_PAY_BILLS.sql
+++ Hotel Management System/11_PR_PAY_BILLS.sql
@@ -7,7 +7,7 @@ DELIMITER $$
 CREATE PROCEDURE `PR_PAY_BILLS`(in orderSno int,out message varchar(30))
 BEGIN
 if exists(select order_no from bill_details where date(order_date)=curdate() and order_no=orderSno) then
-update bill_details set bill_status = 1 where order_no=orderSno and date(order_date)=curdate();
+update bill_details set bill_status = 1,total_price=(select FN_GET_BILL_AMOUNT(orderSno)) where order_no=orderSno and date(order_date)=CURDATE();
 set message="Your bill has been paid";
 else
 set message='Enter a valid order Number';
---
+++ Hotel Management System/14_FN_GET_BILL_AMOUNT.sql
@@ -0,0 +1,14 @@
+/* function  structure for function  `FN_GET_BILL_AMOUNT` */
+
+drop function if exists `FN_GET_BILL_AMOUNT`;
+delimiter $$
+
+create function `FN_GETBIL_AMOUNT`(orderSno int) returns int(11)
+begin
+    declare cost int;
+    declare billSno int;
+    select id into billSno from bill_details where order_no=orderSno and date(order_date)=curdate();
+select sum(qty_no*food_items.price) into cost from order_details join food_items on food_items.`id`=order_details.`item_id` where order_details.`bill_no`=billSno group by order_details.`bill_no`;
+return cost;
+    end $$
+delimiter ;
