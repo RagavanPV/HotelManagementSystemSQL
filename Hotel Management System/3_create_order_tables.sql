babfcf411e12b82a405d6a26fd8bc7e1e13fba1c
Ragavan <ragav87@ymail.com>
2017-01-22 21:53:52 +0530

    Added some missed files

--- Hotel Management System/3_create_order_tables.sql
+++ Hotel Management System/3_create_order_tables.sql
@@ -8,10 +8,11 @@ CREATE TABLE `bill_details` (
   `seat_id` bigint(20) NOT NULL,
   `order_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `bill_status` tinyint(1) NOT NULL DEFAULT '0',
+  `total_price` bigint(20) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `fk_seat_bill` (`seat_id`),
   CONSTRAINT `fk_seat_bill` FOREIGN KEY (`seat_id`) REFERENCES `hotel_seat` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
 
 /*Table structure for table `order_details` */
 
@@ -30,3 +31,17 @@ CREATE TABLE `order_details` (
   CONSTRAINT `fk_bill_no` FOREIGN KEY (`bill_no`) REFERENCES `bill_details` (`id`),
   CONSTRAINT `fk_item_id` FOREIGN KEY (`item_id`) REFERENCES `food_items` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `order_limit` */
+
+DROP TABLE IF EXISTS `order_limit`;
+
+CREATE TABLE `order_limit` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `max_no` int(5) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `order_limit` */
+
+insert  into `order_limit`(`id`,`max_no`) values (1,5);
