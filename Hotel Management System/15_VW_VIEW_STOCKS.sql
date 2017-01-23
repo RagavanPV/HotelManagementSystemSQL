d10e41e657560b25bfa8aa97fad5601ee89dc119
Ragavan <ragav87@ymail.com>
2017-01-23 09:42:42 +0530

    Changed the VW_VIEW_STOCKS

--- Hotel Management System/15_VW_VIEW_STOCKS.sql
+++ Hotel Management System/15_VW_VIEW_STOCKS.sql
@@ -1,6 +1,10 @@
 CREATE VIEW `hms`.`VW_VIEW_STOCKS` 
     AS
-(SELECT items,qty-IFNULL((SELECT SUM(qty_no) FROM order_details WHERE DATE(order_date)=CURDATE() AND STATUS="Ordered" AND item_id=food_schedule.`item_id`  GROUP BY item_id),0) AS Remaining FROM food_schedule 
-LEFT JOIN food_types ON  food_types.id=type_id 
-LEFT JOIN food_items ON food_items.`id`=food_schedule.`item_id`
-GROUP BY food_schedule.id);
+(SELECT TYPE,items ,qty, IFNULL(SUM(qty_no),0) AS Sold, qty-IFNULL(SUM(qty_no),0) AS Remaining FROM food_schedule fs
+LEFT JOIN food_items fi ON fs.`item_id`=fi.`id`
+LEFT JOIN food_types ft ON fs.`type_id`=ft.`id`
+LEFT JOIN order_details od ON od.`item_id`=fi.`id` 
+AND EXISTS(SELECT 1 FROM food_types WHERE id=ft.id AND TIME(od.order_date) BETWEEN time_start AND time_end)
+AND DATE(od.order_date)=CURDATE()
+GROUP BY fi.id,fs.`type_id`
+ORDER BY ft.id ASC ,fi.id ASC);
